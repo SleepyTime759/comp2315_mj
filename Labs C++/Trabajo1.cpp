fe6942d6236662e8c858149943bb06cff7714613
Sebastian Roman <sebitaltalroman@gmail.com>
2024-03-19 10:09:46 -0400

    test

---
+++ Labs C++/Trabajo1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+double num1, num2, media;
+
+cout <<"introduzca el primer numero: ";
+cin >> num1;
+cout <<"introduzca el segundo numero: ";
+
+media = (num1 + num2)/2;
+
+cout<<"La media es: " <<media<<endl;
+
+return 0;
+}
